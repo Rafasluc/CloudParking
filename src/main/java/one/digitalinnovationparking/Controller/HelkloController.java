683b03bb9a6c3cce497ebb81c6d3c3fab19917b2
Rafasluc <rafael-lucena@live.com>
2022-11-12 08:20:57 -0300

    Estrutura Inicial

---
+++ src/main/java/one/digitalinnovationparking/Controller/HelkloController.java
@@ -0,0 +1,14 @@
+package one.digitalinnovationparking.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelkloController {
+    @GetMapping
+    public String hello(){
+        return "hello DIO dev tools";
+    }
+}
